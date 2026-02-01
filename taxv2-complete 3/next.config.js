2cd4fa34bd387d219e0148e314b2c4d8c13226e8
pathansen27-pixel <pathansen27@gmail.com>
2026-02-01 18:35:16 -0500

    Delete taxv2-complete 3 directory

--- taxv2-complete 3/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@vercel/postgres'],
-  },
-};
-
-module.exports = nextConfig;
